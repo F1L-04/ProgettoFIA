0a2030fc4a54270b7973f10b4fd605e0aec77e47
davides003 <d.santillo@studenti.unisa.it>
2025-01-20 15:05:58 +0000

    Dataset3

--- dataset.py
+++ dataset.py
@@ -10,18 +10,45 @@ spotify_dataset
 
 
 # Rimuovere colonne specifiche (ad esempio, 'colonna1' e 'colonna2')
-df = spotify_dataset.drop(['artist_count', 'in_spotify_charts', 'in_apple_charts', 'in_deezer_charts', 'in_shazam_charts', 'released_month', 'released_day', 'key', 'mode'], axis=1)
+df = spotify_dataset.drop(['artist_count', 'in_spotify_charts', 'in_apple_charts', 'in_deezer_charts', 'in_shazam_charts', 'released_month', 'released_day'], axis=1)
+
+#Controllo Valori nulli
+nan_mask = df.isna()
+nan_count = nan_mask.sum()
+
+
+print( nan_count)
+
+#Stampa tutte le righe
+#pd.set_option("display.max_rows", None)
+
+
+#Eliminazione righe con valori nulli
+df.dropna(inplace= True)
+
+#Verifica della corrette eliminazione delle righe nulle 
+nan_mask = df.isna()
+nan_count = nan_mask.sum()
+
+
+print( nan_count)
+
+print(df.describe)
 
 # Dividi il dataset in 70% training e 30% testing
 train_set, test_set = train_test_split(df, test_size=0.3, random_state=42)
 
 # Verifica le dimensioni dei dataset
-print(f"Dimensioni train_set: {train_set.shape}")
-print(f"Dimensioni test_set: {test_set.shape}")
+#print(f"Dimensioni train_set: {train_set.shape}")
+#print(f"Dimensioni test_set: {test_set.shape}")
+
 
 # Esporta i due dataset se necessario
 train_set.to_csv('train_set.csv', index=False)
 test_set.to_csv('test_set.csv', index=False)
 
-print(train_set)
-print(test_set)
+#print(train_set)
+#print(test_set)
+
+
+
